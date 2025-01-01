48906e7b863c6b6550c576cb251807dacb8fbaeb
Your Name <you@example.com>
2025-01-01 14:14:10 +0500

    test auth options

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,5 @@
+"use server";
+
 import NextAuth from "next-auth";
 import VKProvider from "next-auth/providers/vk";
 import YandexProvider from "next-auth/providers/yandex";
@@ -5,12 +7,12 @@ import YandexProvider from "next-auth/providers/yandex";
 const authOptions = {
   providers: [
     VKProvider({
-      clientId: String(process.env.VK_CLIENT_ID),
-      clientSecret: String(process.env.VK_CLIENT_SECRET),
+      clientId: String(process.env.NEXT_PUBLIC_VK_CLIENT_ID),
+      clientSecret: String(process.env.NEXT_PUBLIC_VK_CLIENT_SECRET),
     }),
     YandexProvider({
-      clientId: String(process.env.YANDEX_CLIENT_ID),
-      clientSecret: String(process.env.YANDEX_CLIENT_SECRET),
+      clientId: "b5348efe229e452f9a7d31914dcfc932",
+      clientSecret: "5cee1fa019aa4d3a8f8e447fc978c96a",
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -4,6 +4,13 @@ import React from "react";
 
 import { signIn } from "next-auth/react";
 const SignIn = () => {
+  console.log("VK_CLIENT_ID:", process.env.NEXT_PUBLIC_VK_CLIENT_ID);
+  console.log("VK_CLIENT_SECRET:", process.env.NEXT_PUBLIC_VK_CLIENT_SECRET);
+  console.log("YANDEX_CLIENT_ID:", process.env.NEXT_PUBLIC_YANDEX_CLIENT_ID);
+  console.log(
+    "YANDEX_CLIENT_SECRET:",
+    process.env.NEXT_PUBLIC_YANDEX_CLIENT_SECRET
+  );
   return (
     <div
       style={{
