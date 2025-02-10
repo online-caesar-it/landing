54c38b0c66dc685565548f66add1d44ed795d24d
Nick715404 <nikita.vasia2015@gmail.com>
2025-02-10 14:36:18 +0500

    fix: fixed localStorage issue: it's was undefined and I add condition on typeof for client rendering

--- src/shared/local-storage/token.ts
+++ src/shared/local-storage/token.ts
@@ -1,23 +1,25 @@
 const setAccessToken = (token: string) =>
-  localStorage.setItem("access-token", token);
+	localStorage.setItem('access-token', token);
 
-const getAccessToken = () => localStorage.getItem("access-token");
+const getAccessToken = () => {
+	if (typeof window === 'undefined') return null;
+	return localStorage.getItem('access-token');
+};
 
-const deleteAccessToken = () => localStorage.removeItem("access-token");
+const deleteAccessToken = () => localStorage.removeItem('access-token');
 
 const setRefreshToken = (token: string) =>
-  localStorage.setItem("refresh-token", token);
+	localStorage.setItem('refresh-token', token);
 
-const getRefreshToken = () => localStorage.getItem("refresh-token");
+const getRefreshToken = () => localStorage.getItem('refresh-token');
 
-const deleteRefreshToken = () => localStorage.removeItem("refresh-token");
-
+const deleteRefreshToken = () => localStorage.removeItem('refresh-token');
 
 export const localStorageToken = {
-  setAccessToken,
-  getAccessToken,
-  deleteAccessToken,
-  setRefreshToken,
-  getRefreshToken,
-  deleteRefreshToken,
+	setAccessToken,
+	getAccessToken,
+	deleteAccessToken,
+	setRefreshToken,
+	getRefreshToken,
+	deleteRefreshToken,
 };
