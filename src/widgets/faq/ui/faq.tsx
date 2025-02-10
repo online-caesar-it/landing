a1ab58209213fd4fb340b0ce8beeceb1cf1a9f62
Nick715404 <nikita.vasia2015@gmail.com>
2025-02-10 13:09:00 +0500

    fix: fix an indent for faq section

--- src/widgets/faq/ui/faq.tsx
+++ src/widgets/faq/ui/faq.tsx
@@ -4,9 +4,9 @@ import { FullscreenSectionLayout } from '@/widgets/layouts/ui/fullscreen-section
 
 export const Faq = () => {
 	return (
-		<FullscreenSectionLayout className=' bg-linear-section-blue py-[80px]'>
+		<FullscreenSectionLayout className=' bg-linear-section-blue py-[120px]'>
 			<div className='flex justify-center flex-col'>
-				<h2 className='font-intro text-grey-90 text-section-xl text-center leading-[1] lg:mb-8'>
+				<h2 className='font-intro text-grey-90 text-section-xl text-center mb-[70px] leading-[1] lg:mb-8'>
 					ЧАСТЫЕ ВОПРОСЫ
 				</h2>
 				<div className=''>
