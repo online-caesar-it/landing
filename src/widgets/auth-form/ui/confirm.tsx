a892578c79666001a0437fdd7251b150df50d1ff
Nick715404 <nikita.vasia2015@gmail.com>
2025-02-12 11:50:51 +0500

    fix: fixes auth endpoint

--- src/widgets/auth-form/ui/confirm.tsx
+++ src/widgets/auth-form/ui/confirm.tsx
@@ -1,15 +1,15 @@
-"use client";
+'use client';
 
-import { useConfirm } from "../hooks/useConfirm";
+import { useConfirm } from '../hooks/useConfirm';
 
 const Confirm = () => {
-  const { loading, message } = useConfirm();
-  return (
-    <div>
-      <h1>Подтверждение входа</h1>
-      {loading ? <p>Подтверждаем...</p> : <p>{message}</p>}
-    </div>
-  );
+	const { loading, message } = useConfirm();
+	return (
+		<div className='text-white font-intro text-5xl'>
+			<h1>Подтверждение входа</h1>
+			{loading ? <p>Подтверждаем...</p> : <p>{message}</p>}
+		</div>
+	);
 };
 
 export default Confirm;
