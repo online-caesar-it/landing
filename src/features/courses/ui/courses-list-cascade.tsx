748169166125a953c8eee2e2a199d1e0a3508a36
Nick715404 <nikita.vasia2015@gmail.com>
2025-02-20 12:28:01 +0500

    remove server side rendering from courses block;

--- src/features/courses/ui/courses-list-cascade.tsx
+++ src/features/courses/ui/courses-list-cascade.tsx
@@ -1,35 +1,40 @@
+'use client';
+
 import CourseCard from '@/entities/course/ui/course-card';
 import CourseCardContent from '@/entities/course/ui/course-card-content';
-import { fetchDirections } from '@/shared/api';
 import { directionColorPalette, directionIcons } from '@/shared/constants';
+import { useDirections } from '@/shared/hooks';
 import { twMerge } from 'tailwind-merge';
 
-const CoursesListCascade = async ({ className }: { className?: string }) => {
-	const directions = await fetchDirections();
+const CoursesListCascade = ({ className }: { className?: string }) => {
+	const { directions } = useDirections();
 
 	return (
 		<div className={twMerge('relative', className)}>
 			{directions &&
-				directions.reverse().map((direction, index) => {
-					const directionName = direction.name
-						.toLowerCase()
-						.split(' ')
-						.join('');
+				directions
+					.slice()
+					.reverse()
+					.map((direction, index) => {
+						const directionName = direction.name
+							.toLowerCase()
+							.split(' ')
+							.join('');
 
-					return (
-						<CourseCard
-							color={directionColorPalette[directionName]}
-							index={index}
-							key={direction.id}
-						>
-							<CourseCardContent
-								title={direction.name}
-								body={direction.description}
-								image={directionIcons[directionName]}
-							/>
-						</CourseCard>
-					);
-				})}
+						return (
+							<CourseCard
+								color={directionColorPalette[directionName]}
+								index={index}
+								key={direction.id}
+							>
+								<CourseCardContent
+									title={direction.name}
+									body={direction.description}
+									image={directionIcons[directionName]}
+								/>
+							</CourseCard>
+						);
+					})}
 		</div>
 	);
 };
